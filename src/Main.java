04bb7137f5ab059ae0d9a3c92e5cd75d95d6d472
m0d3rn1ca <jaykonstanta@mail.ru>
2024-04-26 11:38:04 +0300

    task 4_2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,43 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.LinkedList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Human h1 = new Human("Абдул", "Евгеньевич", "Бобров", 40);
-        Human h2 = new Human("Абдул", "Евгеньевич", "Бобров", 41);
-        Human h3 = new Human("Иван", "Михайлович", "Короткий", 24);
-        Human h4 = new Human("Пирог", "Глупович", "Ягодов", 4);
-        Human h5 = new Human("Крекер", "-", "Хрустящий", 1);
-        LinkedList<Human> humans = new LinkedList<>();
+        try (FileReader fileReader = new FileReader("test_2_9.txt")) {
+            LinkedList<String> list = new LinkedList<>();
+            Scanner scanner = new Scanner(fileReader);
+            int appCounter = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("Run")) {
+                    list.add(line.substring(4));
+                    System.out.println(line.substring(4));
+                    appCounter += 1;
+                } else if (line.startsWith("Alt")) {
+                    int tabCounter = 0;
+                    boolean isDelete = false;
 
-        humans.add(h1);
-        humans.add(h2);
-        humans.add(h3);
-        humans.add(h4);
-        humans.add(h5);
+                    for (String substring : line.split("\\+"))
+                        if (substring.equals("Tab"))
+                            tabCounter += 1;
+                        else if (substring.equals("Delete"))
+                            isDelete = true;
 
-        Collections.sort(humans);
-        for (Human human : humans)
-            System.out.printf("%s %s %s %s\n", human.first_name, human.second_name, human.last_name, human.age);
+                    int shift = list.size() - (tabCounter % appCounter) - 1;
+                    if (!isDelete || tabCounter != 0) {
+                        String temp = list.remove(shift);
+                        if (!isDelete)
+                            list.add(temp);
+                        else
+                            appCounter--;
+                        System.out.println(temp);
+                    }
+                }
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.toString());
+        }
     }
 }
